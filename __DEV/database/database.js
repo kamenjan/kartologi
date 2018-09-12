d9385921a1fa7b2b811e0cf413518961751133cd
kamenjan <rok.arih@gmail.com>
2018-09-12 22:30:03 +0200

    decouple parser module

--- __DEV/database/database.js
+++ __DEV/database/database.js
@@ -4,6 +4,8 @@ const inspect = require('eyes').inspector({maxLength: false})
 
 // const db = new sqlite3.Database('../data/example.db.sqlite3')
 
+const productionDB = './db.sqlite3'
+
 /* UID generator  */
 const crypto = require('crypto')
 const generateUID = () => Buffer.from(crypto.randomBytes(8)).toString('hex')
@@ -12,7 +14,7 @@ const generateUID = () => Buffer.from(crypto.randomBytes(8)).toString('hex')
 
 function openSQLiteConnection () {
 
-  let db = new sqlite3.Database('../data/example.db.sqlite3', sqlite3.OPEN_READWRITE, (err) => {
+  let db = new sqlite3.Database(productionDB, sqlite3.OPEN_READWRITE, (err) => {
     if (err) return console.error(err.message)
     console.log('Connected to the example.db.sqlite3 SQlite database.')
   })
--- __DEV/wer_parser/parser.js
+++ __DEV/wer_parser/parser.js
@@ -1,54 +1,29 @@
 const fs = require('fs')
 const xml2js = require('xml2js')
-const inspect = require('eyes').inspector({maxLength: false})
 const parser = new xml2js.Parser()
+const werFilePath = '../data/live_example.xml'
 
-let players, matches
+const inspect = require('eyes').inspector({maxLength: false})
 
-const parseXML = (pathToFile) => {
-  fs.readFile(pathToFile, function(err, data) {
-    parser.parseString(data, function (err, data) {
+/* UID generator  */
+const crypto = require('crypto')
+const generateUID = () => Buffer.from(crypto.randomBytes(8)).toString('hex')
 
-      players = data.event.participation[0].person.map( item => item['$'])
-
-      let novArray = []
-
-      matches = data.event.matches[0].round
-      .map( (roundMatches, index, array) => {
-        roundMatches.match.map( match => novArray.push(match['$']))
-      })
-      // processPlayer(novArray, players)
-      console.log(novArray);
+const readWerFile = (pathToFile) => {
+  fs.readFile(pathToFile, (err, string) => {
+    parser.parseString(string, (err, json) => {
+      inspect(structureWerData(json.event))
     })
   })
 }
 
-const parseXML1 = (pathToFile) => {
-  fs.readFile(pathToFile, function(err, data) {
-    parser.parseString(data, function (err, data) {
+const structureWerData = (event) => ({
+  tournament: { ...event['$'] },
+  players: event.participation[0].person.map( item => item['$'] ),
+  rounds: event.matches[0].round.map(round => ({
+    ... round['$'],
+    matches: round.match.map( match => match['$'] )
+  }))
+})
 
-      const rounds = data.event.matches[0].round
-
-      inspect(rounds)
-    })
-  })
-}
-
-const processPlayer = (matches, players) => {
-  players.forEach( player => {
-    matches.forEach( match => {
-      if (player.id === match.person) {
-        // console.log(`winner = ${player.last} ${player.id}`)
-        players.forEach( player => {
-          if (player.id === match.opponent) {
-            // console.log(`opponent = ${player.last} ${player.id}`)
-          }
-        })
-      }
-    })
-  })
-}
-
-
-// parseXML('../data/live_example.xml')
-parseXML1('../data/live_example.xml')
+readWerFile(werFilePath)
