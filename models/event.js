4d484aceef9c6ada9d34466761f520857abffc08
kamenjan <rok.arih@gmail.com>
2018-09-15 01:47:30 +0200

    write event model rounds query (WIP)

--- models/event.js
+++ models/event.js
@@ -7,8 +7,8 @@
  */
 
 const database = require('../database/database')
-
 const { utils } = require('../database/database')
+const { compose } = require('../services/functional')
 
 /* Own functions */
 const insertTournament = tournament => {
@@ -56,8 +56,8 @@ const insertPlayers = (players, tournament) => {
 
     const filterPlayerParams = player => [
       // TODO: spam mode, set back to player.id
-      utils.generateUID(), player.id, player.first, player.last, player.country
-      // utils.generateUID(), utils.generateUID(), player.first, player.last, player.country
+      // utils.generateUID(), player.id, player.first, player.last, player.country
+      utils.generateUID(), utils.generateUID(), player.first, player.last, player.country
     ]
 
     /* Query for inserting players in players table */
@@ -87,43 +87,71 @@ const insertRounds = (rounds, tournament) => {
   return new Promise( (resolve, reject) => {
     const db = database.open()
 
-    const roundFields = () => ['uid', 'date', 'tournament_uid']
-    const fliterRoundFields = round => ({
-      uid: utils.generateUID(),
-      date: 897123469872146, // TODO: convert round.date to timestamp
-      tournament_uid: tournament.eventguid
+    const addUidsToRounds = (uidGenerator, rounds) => {
+      return rounds.map( round => ({
+        uid: uidGenerator(),
+        tournament_uid: tournament.eventguid,
+        ... round
+      }))
+    }
+
+    const addUidsToRoundMatches = (uidGenerator, round) => ({
+      ... round,
+      matches: round.matches.map( match => ({
+        uid: utils.generateUID(),
+        round_uid: round.uid,
+        ... match
+      }))
     })
 
-    const insertRoundQuery =
-      `INSERT INTO rounds(${roundFields().join(',')}) 
-       VALUES(${ roundFields().map(() => '?').join(',') })`
+    const addUids = (uidGenerator, rounds) => {
+      return addUidsToRounds(uidGenerator, rounds)
+      .map( round => addUidsToRoundMatches(uidGenerator, round))
+    }
 
-    const matchFields = () => ['uid', 'round_uid', 'player', 'opponent', 'outcome', 'win', 'loss', 'draw']
-    const fliterMatchFields = (match, round) => ({
-      uid: utils.generateUID(),
-      round_uid: round,
-      player: match.person,
+    const filterRoundFields = round => {
+      const { PlayFormat, teamformat, ...filteredRound } = round
+      return filteredRound
+    }
 
-    })
+    const filterMatchFields = match => {
+      const { winbydrop, ...filteredMatch } = match
+      return filteredMatch
+    }
 
-    const insertMatchQuery =
-      `INSERT INTO rounds(${matchFields().join(',')}) 
-       VALUES(${ matchFields().map(() => '?').join(',') })`
+    const prepareRounds = rounds => {
+      return addUids(utils.generateUID, rounds)
+      .map( round => ({
+        ... round,
+        matches: round.matches.map( match => filterMatchFields(match))
+      }))
+      .map( round => filterRoundFields(round))
+    }
+
+    console.log('------------')
+    __inspect(prepareRounds(rounds))
+    console.log('------------')
 
     db.serialize(() => {
-      rounds
+      prepareRounds(rounds) // NOTE: prepareRound works perfect, all that is left is running queries
+      /* Iterate over and insert rounds */
       .map( round => {
-        console.log(round);
+        /* TODO: when I figure out rounds, copy the solution for matches */
+        // const roundFields = () => ['uid', 'tournament_uid', 'number', 'date']
+        // const insertRoundQuery =
+        //   `INSERT INTO rounds(${roundFields().join(',')})
+        //    VALUES(${ roundFields().map(() => '?').join(',') })`
+        // db.run(insertRoundQuery, params, err => err ? reject(err) : null)
+
+        /* Iterate over and insert matches */
         round.matches.map( match => {
-          return Object.values(fliterMatchFields(match))
+          // const matchFields = () => ['uid', 'round_uid', 'player', 'opponent', 'outcome', 'win', 'loss', 'draw']
+          const insertMatchQuery =
+            // NOTE: this one is dynamic
+            `INSERT INTO rounds(${Object.keys(match)})
+             VALUES(${ matchFields().map(() => '?').join(',') })` // TODO: make this one dynamic
+          // db.run(insertMatchQuery, Object.values(match), err => err ? reject(err) : null)
         })
-        .map( params => {
-          db.run(insertMatchQuery, params, err => err ? reject(err) : null)
-        })
-        return Object.values(fliterRoundFields(round))
-      })
-      .map( params => {
-        db.run(insertRoundQuery, params, err => err ? reject(err) : null)
       })
       db.close( () => resolve() )
     })
