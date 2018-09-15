5b04b7383d50e9b019f0b86bc5277c1ab2de6470
kamenjan <rok.arih@gmail.com>
2018-09-15 23:05:19 +0200

    update wer parser (WIP)

--- services/wer_parser.js
+++ services/wer_parser.js
@@ -15,15 +15,66 @@ const readWerFile = (werFilePath) => {
   })
 }
 
-const structureEventData = (event) => {
-  return {
-    tournament: { ...event['$'] },
-    players: event.participation[0].person.map( item => item['$'] ),
-    rounds: event.matches[0].round.map(round => ({
-      ... round['$'],
-      matches: round.match.map( match => match['$'] )
-    }))
-  }
+
+/* TODO: finer data parsing before sending to db */
+const structureEventData = (event) => ({
+  tournament: {
+    uid: event['$'].eventguid,
+    date: event['$'].startdate, // TODO: .toTimestamp
+    roundsTotal: event['$'].numberofrounds,
+    format
+  },
+  players: event.participation[0].person.map( item => item['$'] ),
+  rounds: event.matches[0].round.map(round => ({
+    ... round['$'],
+    matches: round.match.map( match => match['$'] )
+  }))
+})
+
+// NOTE: small and simple functions in order:
+// parse tournament date
+// assign tournament uid to rounds
+// generate rounds uids
+// assign round uid to matches
+// generate matches uids
+
+// NOTE: example of data structure for each event related table (except players_to_tournaments)
+const tournamentRowData = {
+  uid: eventguid,
+  date: startdate.toTimestamp, // startdate = "2018-09-10" TODO: convert
+  format,
+  rounds_total: numberofrounds
+}
+
+const playerRowStructure = {
+  uid: undefined,
+  dci: undefined,
+  name_first: undefined,
+  name_last: undefined,
+  country: undefined
+}
+
+const playerToTournamentRowStructure = {
+  player_uid: undefined,
+  tournament_uid: undefined,
+  deck: undefined
+}
+
+const roundRowStructure = {
+  round_uid: undefined,
+  tournament_uid: undefined,
+  date: undefined
+}
+
+const matchRowStructure = {
+  match_uid: undefined,
+  round_uid: undefined,
+  player: undefined,
+  opponent: undefined,
+  outcome: undefined,
+  win: undefined,
+  loss: undefined,
+  draw: undefined
 }
 
 const getEventDataFromWerFile = (pathToWerFile) => {
